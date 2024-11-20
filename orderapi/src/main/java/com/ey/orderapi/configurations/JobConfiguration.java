dba8d7573f1a59e81e486850fea6289f164a16b5
Parameswari Bala <parameswaribala@gmail.com>
2024-11-20 11:05:00 +0530

    updated

---
+++ orderapi/src/main/java/com/ey/orderapi/configurations/JobConfiguration.java
@@ -0,0 +1,35 @@
+package com.ey.orderapi.configurations;
+
+import com.github.javafaker.Faker;
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class JobConfiguration {
+
+    //@Autowired
+   // private ZeebeClient zeebeClient;
+
+    @JobWorker(type = "generateOrderId",autoComplete = false)
+    public Map<String,Integer> handleOrderIdGeneration(final JobClient jobClient, ActivatedJob activatedJob){
+        Faker faker=new Faker();
+        Map<String,Integer> map=new HashMap<>();
+        map.put("OrderId",faker.random().nextInt(1000,10000));
+        jobClient.newCompleteCommand(activatedJob.getKey())
+                .variables(map)
+                .send()
+                .exceptionally((throwable)->{
+            throw new RuntimeException("Job not found");
+        });
+     return map;
+    }
+}
