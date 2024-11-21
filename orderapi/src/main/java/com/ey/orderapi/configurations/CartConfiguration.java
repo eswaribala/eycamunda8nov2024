72db9444516465e741a23c26ec52630cbc5fc7a2
Parameswari Bala <parameswaribala@gmail.com>
2024-11-21 12:45:04 +0530

    updated

--- orderapi/src/main/java/com/ey/orderapi/configurations/CartConfiguration.java
+++ orderapi/src/main/java/com/ey/orderapi/configurations/CartConfiguration.java
@@ -1,5 +1,6 @@
 package com.ey.orderapi.configurations;
 
+import com.ey.orderapi.models.Product;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
@@ -8,7 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -18,6 +21,39 @@ public class CartConfiguration {
    // @Autowired
    // private ZeebeClient zeebeClient;
     private Map<String,Object> map;
+
+    private static List<Map<String,Product>> productsMap =new ArrayList<>();
+    private static long sequence=1;
+
+
+    @JobWorker(type = "storeInArray", autoComplete = false)
+    public List<Map<String, Product>> handleCartStorage(final JobClient jobClient,ActivatedJob activatedJob){
+
+        map=activatedJob.getVariablesAsMap();
+        Map<String,Product> productMap=new HashMap<>();
+        map.entrySet().stream().forEach((entryset)->{
+            System.out.println(entryset.getKey()+","+entryset.getValue());
+        });
+
+        Product product=new Product();
+        product.setProductName(map.get("select_product").toString());
+        product.setQty(Long.parseLong(map.get("textfield_quantity").toString()));
+        productMap.put("product"+sequence,product);
+        sequence++;
+        productsMap.add(productMap);
+        jobClient.newCompleteCommand(activatedJob.getKey())
+                .variables(productsMap)
+                .send()
+                .exceptionally((throwable)->{
+                    throw new RuntimeException("Job not found");
+                });
+
+
+        return productsMap;
+    }
+
+
+
     @JobWorker(type = "computeTotalCost",autoComplete = false)
     public Map<String,Long> computeCostOfTheProduct(final JobClient jobClient, ActivatedJob activatedJob){
 
@@ -25,7 +61,13 @@ public class CartConfiguration {
         map.entrySet().stream().forEach((entryset)->{
             System.out.println(entryset.getKey()+","+entryset.getValue());
         });
-        long totalCost=Long.parseLong(map.get("textfield_quantity").toString()) *5000;
+        List<Map<String,Product>> listOfProducts = (List<Map<String, Product>>) map.get("productsMap");
+        long totalQty=0L;
+        for(Map<String,Product> map: listOfProducts){
+          totalQty+= map.entrySet().stream().map(p->Long.parseLong(p.getValue().toString())).reduce(0L,Long::sum);
+        }
+
+        long totalCost=totalQty*5000;
         Map<String,Long> computedCostMap=new HashMap<>();
         computedCostMap.put("TotalCost",totalCost);
 
@@ -38,4 +80,7 @@ public class CartConfiguration {
           return computedCostMap;
 
     }
+
+
+
 }
---
+++ orderapi/src/main/java/com/ey/orderapi/models/Product.java
@@ -0,0 +1,13 @@
+package com.ey.orderapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private String productName;
+    private long qty;
+}
