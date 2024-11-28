e0dc62df41cc74efbe731ad672ca0fc8aab7d091
Parameswari Bala <parameswaribala@gmail.com>
2024-11-28 11:05:47 +0530

    updated

---
+++ sagaorderapi/src/main/java/com/ey/orderapi/OrderapiApplication.java
@@ -0,0 +1,15 @@
+package com.ey.orderapi;
+
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Deployment(resources ={ ("classpath*:/processes/**/*.bpmn"),("classpath*:/decisions/**/*.dmn")})
+public class OrderapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderapiApplication.class, args);
+    }
+
+}
---
+++ sagaorderapi/src/main/java/com/ey/orderapi/services/OrderService.java
@@ -0,0 +1,8 @@
+package com.ey.orderapi.services;
+
+import com.ey.orderapi.models.Order;
+
+public interface OrderService {
+
+    Order addOrder(Order order);
+}
